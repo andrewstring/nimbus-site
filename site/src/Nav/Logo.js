8dd3e3e2bb7916e4ff979719b512d4061513376c
Andrew Stringfellow <abreezemail@gmail.com>
2023-08-18 17:50:35 -0400

    added noselect and set halo svg to noselect

--- site/src/Nav/Logo.js
+++ site/src/Nav/Logo.js
@@ -5,7 +5,7 @@ import "../css/Logo.css"
 const Logo = (props) => {
     return (
         <div className="logo">
-            <img src="/assets/halo.svg" alt="Nimbus Halo"></img>
+            <img className="noselect" src="/assets/halo.svg" alt="Nimbus Halo"></img>
             <h1 className="logo-text">NIMBUS</h1>
         </div>
     )
