125bd6d91ead42427cd3423974a971b67f8d5ab5
Andrew Stringfellow <abreezemail@gmail.com>
2023-08-28 23:42:11 -0400

    added nested grid for find and contact components...TODO:Continue formatting contact component

--- site/src/Components/Content/Contact.js
+++ site/src/Components/Content/Contact.js
@@ -4,14 +4,16 @@ import '../../css/Contact.css'
 
 const Contact = (props) => {
     return (
-        <div className="contact">
-            <form>
-                <input className="first-name" type="text" name="first-name" placeholder="First Name"></input>
-                <input className="last-name" type="text" name="last-name" placeholder="Last Name"></input>
-                <input className="email" type="text" name="email" placeholder="Email"></input>
-                <input className="submit" type="submit" value="Submit"></input>
-            </form>
-            <h4>SHOOT</h4>
+        <div className="contact-outer">
+            <div className="contact-inner">
+                <form>
+                    <input className="first-name" type="text" name="first-name" placeholder="First Name"></input>
+                    <input className="last-name" type="text" name="last-name" placeholder="Last Name"></input>
+                    <input className="email" type="text" name="email" placeholder="Email"></input>
+                    <input className="submit" type="submit" value="Submit"></input>
+                </form>
+                <h4>SHOOT</h4>
+            </div>
         </div>
     )
 }
